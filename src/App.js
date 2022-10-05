e61ca9f940b9a713b0f91a9373c4939110b8a3d9
Joanna Panimasz <pani.joanna93@gmail.com>
2022-10-05 16:07:12 +0200

    class changed its name to className

--- src/App.js
+++ src/App.js
@@ -2,40 +2,40 @@
 
 function App() {
   return (
-    <div className="App">
-    <div class="body__container">
-        <form class="form">
-            <fieldset class="form__fieldset">
-                <legend class="form__legend">Kalkulator walut</legend>
+    <div classNameName="App">
+    <div className="body__container">
+        <form className="form">
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">Kalkulator walut</legend>
                 <p>
                     <label>
-                        <span class="form__labelText">
+                        <span className="form__labelText">
                             Kwota w zł*:
                         </span>
-                        <input class="form__field" required type="number" name="kwota" step="0.01" min="1"
+                        <input className="form__field" required type="number" name="kwota" step="0.01" min="1"
                             max="1000000000"/>
                     </label>
                 </p>
                 <p>
                     <label>
-                        <span class="form__labelText">
+                        <span className="form__labelText">
                             Wynik:
                         </span>
-                        <input class="form__field" readonly type="number" name="wynik" step="0.01" min="1"
+                        <input className="form__field" readonly type="number" name="wynik" step="0.01" min="1"
                             max="1000000000"/>
                     </label>
                 </p>
-                <p class="form__paragraph">
+                <p className="form__paragraph">
                     * - pole obowiązkowe
                 </p>
                 <label>
-                    <select class="form__option">
+                    <select className="form__option">
                         <option value="EUR">Euro</option>
                         <option value="GBP">Funt brytyjski</option>
                         <option value="USD">Dolar amerykański</option>
                     </select>
                 </label>
-                <button class="form__button">Przelicz walutę</button>
+                <button className="form__button">Przelicz walutę</button>
             </fieldset>
         </form>
     </div>
