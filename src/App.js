778a69d3dbff44a41153883d6f737f24fddb604e
Joanna Panimasz <pani.joanna93@gmail.com>
2022-10-05 15:59:45 +0200

    html added to a function App

--- src/App.js
+++ src/App.js
@@ -3,20 +3,42 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="body__container">
+        <form class="form">
+            <fieldset class="form__fieldset">
+                <legend class="form__legend">Kalkulator walut</legend>
+                <p>
+                    <label>
+                        <span class="form__labelText">
+                            Kwota w zł*:
+                        </span>
+                        <input class="form__field" required type="number" name="kwota" step="0.01" min="1"
+                            max="1000000000"/>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span class="form__labelText">
+                            Wynik:
+                        </span>
+                        <input class="form__field" readonly type="number" name="wynik" step="0.01" min="1"
+                            max="1000000000"/>
+                    </label>
+                </p>
+                <p class="form__paragraph">
+                    * - pole obowiązkowe
+                </p>
+                <label>
+                    <select class="form__option">
+                        <option value="EUR">Euro</option>
+                        <option value="GBP">Funt brytyjski</option>
+                        <option value="USD">Dolar amerykański</option>
+                    </select>
+                </label>
+                <button class="form__button">Przelicz walutę</button>
+            </fieldset>
+        </form>
+    </div>
     </div>
   );
 }
