e21df281df5e6243a2c54c013bb078ca55c03f5a
Joanna Panimasz <pani.joanna93@gmail.com>
2022-10-14 11:35:22 +0200

    name added to currencies array

--- src/Currencies/index.js
+++ src/Currencies/index.js
@@ -1,15 +1,19 @@
 
 const Currencies = [
     {
+        name: "Euro",
         content: "EUR",
         rate: 4.83
     },
     {
+        name: "Funt brytyjski",
         content: "GBP",
         rate: 5.59
     },
     {
+        name: "Dolar amerykański",
         content: "USD",
         rate: 4.96
     }
 ];
+
