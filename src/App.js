77efdd907b73534dea8913ed93029ed94fe51171
Joanna Panimasz <pani.joanna93@gmail.com>
2022-10-14 12:34:21 +0200

    function App changed

--- src/App.js
+++ src/App.js
@@ -2,25 +2,9 @@ import Form from "./Form";
 import {useState} from "react";
 
 function App() {
-    const [result, setResult] = useState(0);
-    const [amount, setAmount] = useState();
-    const [currencies, setCurrencies] = useState([
-        {
-            id: 1,
-            content: "EUR",
-            rate: 4.85
-        }, 
-        {
-            id: 2,
-            content: "USD",
-            rate: 5.0
-        }, 
-        {
-            id: 3,
-            content: "GBP",
-            rate: 5.52
-        }
-    ]);
+    const [result, setResult] = useState();
+    const [fromCurrency, setFromCurrency] = useState();
+    const [toCurrency, setToCurrency] = useState();
 
 
     return (
