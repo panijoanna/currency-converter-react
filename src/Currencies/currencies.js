f863790d6fcc34d3b414051782cc19056dd21bc4
Joanna Panimasz <pani.joanna93@gmail.com>
2022-10-14 12:33:25 +0200

    index.js changed name to currencies.js

---
+++ src/Currencies/currencies.js
@@ -0,0 +1,19 @@
+
+export const currencies = [
+    {
+        name: "Euro",
+        content: "EUR",
+        rate: 4.83
+    },
+    {
+        name: "Funt brytyjski",
+        content: "GBP",
+        rate: 5.59
+    },
+    {
+        name: "Dolar amerykański",
+        content: "USD",
+        rate: 4.96
+    }
+];
+
