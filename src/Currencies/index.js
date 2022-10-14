dc2842a8b02661d9e5480af93a40d117a03e0fd4
Joanna Panimasz <pani.joanna93@gmail.com>
2022-10-14 11:19:11 +0200

    array with currencies added to index.js in Currencies

---
+++ src/Currencies/index.js
@@ -0,0 +1,15 @@
+
+const Currencies = [
+    {
+        content: "EUR",
+        rate: 4.83
+    },
+    {
+        content: "GBP",
+        rate: 5.59
+    },
+    {
+        content: "USD",
+        rate: 4.96
+    }
+];
