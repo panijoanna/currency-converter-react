3593e4d77175ad5daf0ccee1ffec0e42a3edc7b2
Joanna Panimasz <pani.joanna93@gmail.com>
2022-10-15 12:48:54 +0200

    const calculateResult, insertResult and onFormSubmit added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,60 +1,74 @@
 import "./style.css";
 
-const Form = ({ insertResult, calculateResult }) => {
-    const [currency, setCurrency] = useState();
+const Form = ({ result, calculateResult }) => {
+    const [currency, setCurrency] = useState(0);
     const [amount, setAmount] = useState("");
+    const [result, setResult] = useState();
+
+    const calculateResult = (currency, amount) => {
+        setResult(amount / exchangeRate);
+    };
+
+    const insertResult = () => {
+
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(currency, amount);
+    };
 }
 
-    return (
-        <form className="form">
-            <fieldset className="form__fieldset">
-                <legend className="form__legend">Kalkulator walut</legend>
-                <p>
-                    <label>
-                        <span className="form__labelText">
-                            Kwota w zł*:
-                        </span>
-                        <input
-                            className="form__field"
-                            required
-                            type="number"
-                            name="kwota"
-                            step="0.01"
-                            min="1"
-                            max="1000000000"/>
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <span className="form__labelText">
-                            Wynik:
-                        </span>
-                        <input
-                            className="form__field"
-                            readonly
-                            type="number"
-                            name="wynik"
-                            step="0.01"
-                            min="1"
-                            max="1000000000"/>
-                    </label>
-                </p>
-                <p className="form__paragraph">
-                    * - pole obowiązkowe
-                </p>
+return (
+    <form 
+        className="form" 
+        onFormSubmit={onFormSubmit}>
+        <fieldset className="form__fieldset">
+            <legend className="form__legend">Kalkulator walut</legend>
+            <p>
                 <label>
-                    <select className="form__option">
-                        <option value="EUR">Euro</option>
-                        <option value="GBP">Funt brytyjski</option>
-                        <option value="USD">Dolar amerykański</option>
-                    </select>
+                    <span className="form__labelText">
+                        Kwota w zł*:
+                    </span>
+                    <input
+                        className="form__field"
+                        required
+                        type="number"
+                        name="kwota"
+                        step="0.01"
+                        min="1"
+                        max="1000000000"/>
                 </label>
-                <button 
-                    onClick={insertResult} 
-                    className="form__button">Przelicz walutę
-                </button>
-            </fieldset>
-        </form>
-    );
+            </p>
+            <p>
+                <label>
+                    <span className="form__labelText">
+                        Wynik:
+                    </span>
+                    <input
+                        className="form__field"
+                        readonly
+                        type="number"
+                        name="wynik"
+                        step="0.01"
+                        min="1"
+                        max="1000000000"/>
+                </label>
+            </p>
+            <p className="form__paragraph">
+                * - pole obowiązkowe
+            </p>
+            <label>
+                <select className="form__option">
+                    <option value="EUR">Euro</option>
+                    <option value="GBP">Funt brytyjski</option>
+                    <option value="USD">Dolar amerykański</option>
+                </select>
+            </label>
+            <button onClick={insertResult} className="form__button">Przelicz walutę
+            </button>
+        </fieldset>
+    </form>
+);
 
 export default Form;
