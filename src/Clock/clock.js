fa5a7f1dcb4c42bb3648160ff97f64e07b39841c
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-02 15:43:03 +0100

    add new Date() to useState hook

--- src/Clock/clock.js
+++ src/Clock/clock.js
@@ -2,5 +2,5 @@ import { useState, useEffect } from "react";
 import "./style.css";
 
 const Clock = () => {
-    const [dateItem, setDateItem] = useState();
+    const [dateItem, setDateItem] = useState(new Date());
 };
