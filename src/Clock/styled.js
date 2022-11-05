f3b2be70a3f2cd635004a47eb1fdffd8872bb5ab
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 17:18:20 +0100

    import and export Clock component

---
+++ src/Clock/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Clock = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding: 0 20px 20px;
+  font-family: "Space Mono", monospace;
+  color: #f6ebff;
+  font-size: 12px;
+`;
