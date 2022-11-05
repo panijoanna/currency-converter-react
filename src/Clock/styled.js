9a5664114923d1649ea351e423ed34d59b286d1d
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 17:21:09 +0100

    rename Clock to StyledClock

--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Clock = styled.div`
+export const StyledClock = styled.div`
   display: flex;
   justify-content: flex-end;
   padding: 0 20px 20px;
