f01666ef091164122ca116931978cd81b4c2b9b0
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 17:29:05 +0100

    export FormFieldset

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -4,3 +4,11 @@ export const StyledForm = styled.form`
   margin: 40px;
   border-radius: 10px;
 `;
+
+export const FormFieldset = styled.fieldset`
+  border: 2px solid #f1f8f8;
+  padding: 30px 80px 80px 80px;
+  border-radius: 5px;
+  max-width: 800px;
+  margin: auto;
+`;
