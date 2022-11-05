c85e6f218e8b65a2c3bdaac9ae252bbab87cd055
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-05 17:35:55 +0100

    export FormLegend and FormButton

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -12,3 +12,31 @@ export const FormFieldset = styled.fieldset`
   max-width: 800px;
   margin: auto;
 `;
+
+export const FormLegend = styled.legend`
+  font-weight: 700;
+  font-size: 18px;
+  border-radius: 5px;
+  padding: 10px;
+  color: #f6ebff;
+  text-shadow: 2px 2px 0px #4b4b4d;
+`;
+
+export const FormButton = styled.button`
+  border: none;
+  font-weight: 500;
+  background-color: #f1f8f8;
+  color: #808080;
+  border-radius: 5px;
+  padding: 10px;
+  margin-top: 10px;
+  width: 100%;
+
+  &:hover {
+    background-color: #d1e2e2;
+  }
+
+  &:active {
+    background-color: #f9f871;
+  }
+`;
