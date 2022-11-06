520ec192349568596da626612e8740eb0c52ea03
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-06 14:18:47 +0100

    import and export GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./background.png";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+  }
+  
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+  
+  body {
+    font-family: "Montserrat", sans-serif;
+    font-weight: 300;
+    line-height: 1.5;
+    font-size: 16px;
+    margin: 0px auto;
+    background-image: url("${background}");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    height: 100%;
+  }
+`;
