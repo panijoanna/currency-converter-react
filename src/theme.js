35bcc7d253442521caefa43e439b6b3cf653f36c
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-07 15:23:09 +0100

    add theme colors to theme.js

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    color: {
+        chalk: "#f6ebff",
+        catskill: "#f1f8f8",
+        gray: "#808080",
+        abbey: "#4b4b4d",
+        peach: "#faf5f5",
+    }
+};
