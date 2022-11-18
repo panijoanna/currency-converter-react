16b39820ed9c8accebe793fa55450a11abf9f75d
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-18 11:58:50 +0100

    remove Error Component

--- src/Error/error.js
+++
@@ -1,9 +0,0 @@
-import { StyledError } from "./styled";
-
-const Error = () => {
-<StyledError>
-    Coś poszło nie tak. Sprawdź połączenie z internetem.
-</StyledError>
-};
-
-export default Error;
--- src/Error/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const StyledError = styled.p`
-  display: flex;
-  justify-content: center;
-  font-size: 18px;
-`;
