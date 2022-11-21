7103d3ead76c49ff1334d5885e3e47d4621abb76
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-21 15:05:47 +0100

    import useState to useRates custom hook

--- src/Form/Result/useRates.js
+++ src/Form/Result/useRates.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const useRates = () => {
     const [rates, setRates] = useState({ state: "loading" });
