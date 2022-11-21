091cde501e9363cec9b245e1c2364659bdac8d3b
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-21 15:29:59 +0100

    add baseURL

--- src/Form/Result/useRates.js
+++ src/Form/Result/useRates.js
@@ -5,12 +5,11 @@ export const useRates = () => {
     const [rates, setRates] = useState({ state: "loading" });
 
 const baseURL = "https://api.exchangerate.host/latest?base=PLN";
-};
 
 useEffect(() => {
     const getData = async () => {
         try {
-            const response = await axios.get();
+            const response = await axios.get(baseURL);
             const { rates, date } = response.data;
 
             setRates({
