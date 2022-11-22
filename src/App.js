d8f54d99a9d0c10ccce40fab19719f95c83a8c92
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-22 15:10:15 +0100

    remove calculateResult from App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import { useState } from "react";
 import { currencies } from "./currencies/currencies";
 
 function App() {
-  const [result, setResult] = useState(0);
-
-  const calculateResult = (currency, amount) => {
-    const exchangeRate = currencies.find((el) => el.content === currency);
-    const rate = exchangeRate.rate;
-
-    setResult((+amount / rate).toFixed(2));
-  };
+  
 
   return (
     <>
