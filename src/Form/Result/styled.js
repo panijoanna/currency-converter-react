0bbfb99dcb9c898acc737a3f2b6d28ce8a0a36d2
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-22 15:05:21 +0100

    add color to the styled.js in Result

--- src/Form/Result/styled.js
+++ src/Form/Result/styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Loading = styled.p`
 font-size: 18px;
 font-weight: 500;
+color: ${({ theme }) => theme.color.casablanca};
 `;
 
 export const Error = styled.p`
