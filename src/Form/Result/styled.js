4fbc3b2b0170710e16fe43ef8f2d3fc682fe8ae1
Joanna Panimasz <pani.joanna93@gmail.com>
2022-11-22 15:06:37 +0100

    add text-align in Error

--- src/Form/Result/styled.js
+++ src/Form/Result/styled.js
@@ -9,5 +9,6 @@ color: ${({ theme }) => theme.color.casablanca};
 export const Error = styled.p`
 font-size: 18px;
 font-weight: 500;
+text-align: center;
 color: ${({ theme }) => theme.color.copper};
 `;
