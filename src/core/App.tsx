82b1e988aabba2af0c47d21b37e4f5e967c6edb4
Joanna Panimasz <pani.joanna93@gmail.com>
2023-11-28 21:25:39 +0100

    feat: convert App to ts + remove unused props

---
+++ src/core/App.tsx
@@ -0,0 +1,13 @@
+import Form from "../features/Form";
+import Clock from "../features/Clock/clock";
+
+function App() {
+  return (
+    <>
+      <Clock />
+      <Form />
+    </>
+  );
+}
+
+export default App;
